714c73e6f0b3bf9ea993776c52afc58eefeaaca1
krishna <121710803027@gitam.in>
2022-11-10 18:45:50 +0530

    prep web

---
+++ defence academy 2_files/scrollView.js
@@ -0,0 +1,9 @@
+// <!-- defence button scroll js program  -->
+
+const element = document.getElementById("cmtemh");
+
+function myFunction() {
+    element.scrollIntoView();
+}
+
+// <!-- scroll program ends here  -->
